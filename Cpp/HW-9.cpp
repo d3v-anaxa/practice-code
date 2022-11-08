3659f32cc9a55c5c2fdd891d5384da3a25cca207
d3v-anaxa <d3v.anaxa@gmail.com>
2022-11-08 16:10:28 +0530

    ATM program

--- Cpp/HW-9.cpp
+++ Cpp/HW-9.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
@@ -6,10 +7,11 @@ int main()
     int money,noteVal;
     int count100 = 0, count200 = 0, count500 = 0, count2000 = 0;
 
-    std::cout << "\n+-------------------------------ATM------------------------------>\n|" << endl;
-    std::cout << "| How much money do you want to withdraw ? [in Rs.] : " && std::cin >> money ;
+    std::cout << "[ATM]" << endl;
+    std::cout << "\tEnter WIthdrawal Amount [INR] : " && std::cin >> money ;
     
-    if (money % 100 != 0){
+    if (money % 100 != 0 || money < 100){
+        std::cout << "\033[2J\033[1;1H";
         std::cout << "| Only 2000, 500, 200 & 100 rupees notes are available!" << endl;
         return main();
     }
@@ -24,10 +26,10 @@ int main()
         }
         money-=noteVal;
     }
-    std::cout << "|\n+---------------------WITHDRAWAL SUCCESSFULL--------------------->" << endl;
-    std::cout << "| 2000 NOTES\t>\t" << count2000 << endl;
-    std::cout << "| 500 NOTES\t>\t" << count500 << endl;
-    std::cout << "| 200 NOTES\t>\t" << count200 << endl;
-    std::cout << "| 100 NOTES\t>\t" << count100 << endl;
-    std::cout << "+---------------------------------------------------------------->" << endl;
+    std::cout << "[WITHDRAWAL SUCCESSFULL]" << endl;
+    
+    std::cout << "\t2000 NOTES\t>\t" << count2000 << endl;
+    std::cout << "\t500 NOTES\t>\t" << count500 << endl;
+    std::cout << "\t200 NOTES\t>\t" << count200 << endl;
+    std::cout << "\t100 NOTES\t>\t" << count100 << endl;
 }
